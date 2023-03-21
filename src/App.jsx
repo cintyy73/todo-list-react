f172eee675d2a7ff66757d6c75e183a487cbb0ee
cintyy <cintiapaezdm@gmail.com>
2023-03-21 16:21:06 -0300

    Agrego imagenes de fondo en assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+//import { useState } from 'react'
 
+import { Box, Heading } from "@chakra-ui/react"
+
+function App() {
+  // const [background, setBackground] = useState([index])
+  //const images = ["url('./assets/background/fractal-3.jpg')"]
+  
+  // const handdleIndex = () => {
+  //   const index = Math.floor(Math.random()*10)
+  //   if(index === Math.floor(Math.random()*10)){
+  //     index = Math.floor(Math.random()*10)
+  //   }
+  //   setBackground(images[index])
+  //}
+
+  return (
+    <Box >
+      <Heading>hola!</Heading>
+    </Box>
+  )
+}
+
+{/* <button onClick={handdleIndex}>
+click
+</button> */}
+export default App
